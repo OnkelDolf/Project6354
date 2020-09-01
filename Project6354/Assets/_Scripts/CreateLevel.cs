c1f62e12a293cd814877963ac36213d214946780
Onkel Dolf <eric.hederstedtjonsson@elev.ga.lbs.se>
2020-09-01 15:35:09 +0200

    Update
    Update

--- Project6354/Assets/_Scripts/CreateLevel.cs
+++ Project6354/Assets/_Scripts/CreateLevel.cs
@@ -5,46 +5,57 @@ using UnityEngine.UI;
 
 public class CreateLevel : MonoBehaviour
 {
-    public InputField generateX;
-    public InputField generateZ;
+    [SerializeField] private InputField generateX;
+    [SerializeField] private InputField generateZ;
 
     public GameObject nodePrefab;
     public GameObject nodeParent;
-    public Quaternion rotation;
+    public GameObject outlinePrefab;
+    private Quaternion rotation;
     
-    public int maxAllowedGridSize = 30;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private int maxAllowedGridSize = 30;
+    [SerializeField] private int minAllowedGridSize = 5;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            generateLevel();
-        }
-    }
+    [SerializeField] private int outlineOffsetX = 1;
+    [SerializeField] private int outlineOffsetZ = 1;
 
     public void generateLevel()
     {
-        if (int.Parse(generateX.text) <= maxAllowedGridSize && int.Parse(generateZ.text) <= maxAllowedGridSize)
+        // Initialize and convert the text in the input field to integers.
+        int _generateX = int.Parse(generateX.text);
+        int _generateZ = int.Parse(generateZ.text);
+        
+        
+        if (_generateX <= maxAllowedGridSize && _generateZ <= maxAllowedGridSize && _generateX >= minAllowedGridSize && _generateZ >= minAllowedGridSize && generateX.text != null && generateZ.text != null)
         {
-            for (int i = 0; i < int.Parse(generateX.text); i++)
+            // Generate the main platform
+            for (int i = 0; i < _generateX; i++)
             {
-                for (int j = 0; j < int.Parse(generateZ.text); j++)
+                for (int j = 0; j < _generateZ; j++)
                 {
                     Vector3 pos = new Vector3(i * 10, 0, j * 10);
                     Instantiate(nodePrefab, pos, rotation, nodeParent.transform);
                 }
             }
+            
+            // Generate the main platforms outline
+            for (int i = 0; i <= _generateX + 1; i++)
+            {
+                for (int j = 0; j <= _generateZ + 1; j++)
+                {
+                    if (i == 0 || i == _generateX + 1 || j == 0 || j == _generateZ + 1)
+                    {
+                        Vector3 pos = new Vector3(i * 10 - outlineOffsetX * 10, 0, j * 10 - outlineOffsetZ * 10);
+                        Instantiate(outlinePrefab, pos, rotation, nodeParent.transform);
+                    }
+                }
+            }
+            
+            GameObject.FindWithTag("Level Generation UI").SetActive(false);
         }
         else
         {
-            Debug.Log("Gone over max grid size");
+            Debug.Log("CreateLevel.cs: Size input error.");
         }
     }
 }
