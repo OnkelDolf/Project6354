a40abd57d71af07f05ab6d305f2c1b584bd94822
Onkel Dolf <eric.hederstedtjonsson@elev.ga.lbs.se>
2020-09-01 16:00:24 +0200

    Init Grid
    Init Grid

---
+++ Project6354/Assets/_Scripts/RTS/Click.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask clickablesLayer;
+    
+    [HideInInspector]
+    public List<GameObject> selectableObjects;
+    private List<GameObject> selectedObjects;
+
+    private Vector3 startPos;
+    private Vector3 endPos;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        selectedObjects = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) 
+        {
+            foreach (GameObject selected in selectedObjects) 
+            {
+                Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Debug.Log(hit.point.ToString());
+                    selected.GetComponent<CharacterController>().goTo(hit);
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            //startPos = new Vector3();
+            startPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+            RaycastHit rayHit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, clickablesLayer))
+            {
+                Clickable clickableScript = rayHit.collider.GetComponent<Clickable>();
+
+                if (Input.GetKey("left shift"))
+                {
+                    if (clickableScript.selected == false)
+                    {
+                        selectedObjects.Add(rayHit.collider.gameObject);
+                        clickableScript.selected = true;
+                        clickableScript.Clicked();
+                    }
+                    else
+                    {
+                        selectedObjects.Remove(rayHit.collider.gameObject);
+                        clickableScript.selected = false;
+                        clickableScript.Clicked();
+                    }
+                }
+                else
+                {
+                    ClearSelected();
+
+                    selectedObjects.Add(rayHit.collider.gameObject);
+                    clickableScript.selected = true;
+                    clickableScript.Clicked();
+                }
+            }
+            else 
+            {
+                foreach (GameObject obj in selectedObjects)
+                {
+                    obj.GetComponent<Clickable>().selected = false;
+                    obj.GetComponent<Clickable>().Clicked();
+                }
+
+                selectedObjects.Clear();
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0)) 
+        {
+            endPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+            if (startPos != endPos) 
+            {
+                SelectObjects();
+            }
+        }
+    }
+
+    private void SelectObjects() 
+    {
+        List<GameObject> remObjects = new List<GameObject>();
+
+        if (Input.GetKey("left shift") == false) 
+        {
+            ClearSelected();
+        }
+
+        Rect selectRect = new Rect(startPos.x, startPos.y, endPos.x - startPos.x, endPos.y - startPos.y);
+
+        foreach (GameObject selectObject in selectableObjects) 
+        {
+            if (selectObject != null)
+            {
+                if (selectRect.Contains(Camera.main.WorldToViewportPoint(selectObject.transform.position), true))
+                {
+                    selectedObjects.Add(selectObject);
+                    selectObject.GetComponent<Clickable>().selected = true;
+                    selectObject.GetComponent<Clickable>().Clicked();
+                }
+            }
+            else
+            {
+                remObjects.Add(selectObject);
+            }
+        }
+
+        if (remObjects.Count > 0) 
+        {
+            foreach (GameObject rem in remObjects) 
+            {
+                selectableObjects.Remove(rem);
+            }
+
+            remObjects.Clear();
+        }
+    }
+    private void ClearSelected() 
+    {
+        if (selectedObjects.Count > 0)
+        {
+            foreach (GameObject obj in selectedObjects)
+            {
+                obj.GetComponent<Clickable>().selected = false;
+                obj.GetComponent<Clickable>().Clicked();
+            }
+
+            selectedObjects.Clear();
+        }
+    }
+}
---
+++ Project6354/Assets/_Scripts/RTS/Clickable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour
+{
+    [SerializeField]
+    private Material red;
+    [SerializeField]
+    private Material green;
+
+    [HideInInspector]
+    public bool selected = false;
+
+    private MeshRenderer myRend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRend = GetComponent<MeshRenderer>();
+        Camera.main.gameObject.GetComponent<Click>().selectableObjects.Add(this.gameObject);
+        Clicked();
+    }
+
+    public void Clicked() 
+    {
+        if (selected == true)
+        {
+            myRend.material = green;
+        }
+        else 
+        {
+            myRend.material = red;
+        }
+    }
+}
